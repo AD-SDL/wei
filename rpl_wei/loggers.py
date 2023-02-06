224e45918d131c2a6cd607d0c061d6c604dfb6f8
Kyle Hippe <kyle.hippe@gmail.com>
2023-02-06 15:04:24 -0600

    Bug fixes to test for refactor changes

--- rpl_wei/loggers.py
+++ rpl_wei/loggers.py
@@ -41,7 +41,7 @@ class WEI_Logger:
         logger = (
             WEI_Logger._create_logger(
                 log_name,
-                log_dir.log_dir / f"{log_name}.log",
+                log_dir / f"{log_name}.log",
                 log_level,
             )
             if not logging.getLogger().hasHandlers()
