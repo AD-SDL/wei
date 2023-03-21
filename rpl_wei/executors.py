cce6b74d655969d1c7f7526db9e4ef66173af770
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-03-20 19:18:16 -0500

    tcp place holders

--- rpl_wei/executors.py
+++ rpl_wei/executors.py
@@ -69,18 +69,30 @@ def wei_camera_callback(step: Step, **kwargs):
     )
     __kill_node()
 
+def wei_tcp_callback(step: Step, **kwargs):
+    from socket import socket
+    module: Module = kwargs["step_module"]
+
+    sock = socket.bind(module.config["tcp_address"],module.config["tcp_port"])
+    msg = {
+        "action_handle": step.command,
+        "action_vars": step.args,
+    }
+
+    socket.send(str(msg).encode())
+    answer = socket.read().decode()
+    return 
 
 def silent_callback(step: Step, **kwargs):
     print(step)
 
 
 ### Executor mapping ###
-
-
 class Executor_Map:
     function = {
         "wei_ros_node": wei_service_callback,
         "wei_ros_camera": wei_camera_callback,
+        "wei_tcp_node": wei_tcp_callback,
         "silent_callback": silent_callback,
     }
 
