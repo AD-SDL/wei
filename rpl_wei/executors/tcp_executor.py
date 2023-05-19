3be4118062f5cc47e58328004e9c685074d1ed88
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-05-19 14:17:18 -0500

    add socket close to tcp_executor

--- rpl_wei/executors/tcp_executor.py
+++ rpl_wei/executors/tcp_executor.py
@@ -19,4 +19,5 @@ def wei_tcp_callback(step: Step, **kwargs):
     action_msg = tcp_response.get('action_msg')
     action_log = tcp_response.get('action_log')
     #TODO: assert all of the above. deal with edge cases?
+    sock.close()
     return action_response, action_msg, action_log
