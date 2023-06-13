b8bc7e601453da59b76ccc801dfff83261984762
Dozgulbas <dozgulbas@anl.gov>
2023-06-13 13:59:34 -0500

    debugging bio

---
+++ rpl_wei/core/executors/step_executor.py
@@ -0,0 +1,82 @@
+"""Handling execution for steps in the RPL-SDL efforts"""
+import logging
+from typing import Callable, List, Optional
+
+from rpl_wei.core.data_classes import Module, Step, StepStatus
+from rpl_wei.core.executors.rest_executor import wei_rest_callback
+from rpl_wei.core.executors.ros2_executor import (
+    wei_ros2_camera_callback,
+    wei_ros2_service_callback,
+)
+from rpl_wei.core.executors.tcp_executor import wei_tcp_callback
+
+
+def silent_callback(step: Step, **kwargs):
+    """Placeholder"""
+    print(step)
+    return "silent", step.command, ""
+
+
+########################
+#   Executor mapping   #
+########################
+
+
+class Executor_Map:
+    """Placeholder"""
+    function = {
+        "wei_ros_node": wei_ros2_service_callback,
+        "wei_ros_camera": wei_ros2_camera_callback,
+        "wei_tcp_node": wei_tcp_callback,
+        "wei_rest_node": wei_rest_callback,
+        "wei_zmq_node"
+        "simulate_callback": silent_callback,
+    }
+
+
+class StepExecutor:
+    """Class to handle executing steps"""
+
+    def execute_step(
+        self,
+        step: Step,
+        step_module: Module,
+        callbacks: Optional[List[Callable]] = None,
+        logger: Optional[logging.Logger] = None,
+        simulate: bool = False,
+    ) -> StepStatus:
+        """Executes a single step from a workflow
+
+        Parameters
+        ----------
+        step : Step
+            A single step from a workflow definition
+
+        Returns
+        -------
+        StepStatus
+            A status of the step (in theory provides async support with IDLE, RUNNING, but for now is just SUCCEEDED/FAILED)
+        """
+        assert (
+            step_module.type in Executor_Map.function
+        ), f"Executor not found for {step_module.type}"
+
+        logger.info(f"Started running step with name: {step.name}")
+        logger.debug(step)
+
+        # map the correct executor function to the step_module
+        if simulate:
+            action_response, action_msg, action_log = Executor_Map.function[
+                "simulate_callback"
+            ](step, step_module=step_module)
+        else:
+            action_response, action_msg, action_log = Executor_Map.function[
+                step_module.type
+            ](step, step_module=step_module)
+
+        logger.info(f"Finished running step with name: {step.name}")
+
+        if not action_response:
+            action_response = StepStatus.SUCCEEDED
+
+        return action_response, action_msg, action_log
--- rpl_wei/core/executors/tcp_executor.py
+++ rpl_wei/core/executors/tcp_executor.py
@@ -20,7 +20,7 @@ def wei_tcp_callback(step: Step, **kwargs):
     tcp_response = sock.recv(1024).decode()
     tcp_response = json.loads(tcp_response)
     print(tcp_response)
-    action_response = json.loads(tcp_response.get("action_response"))
+    action_response = tcp_response.get("action_response")
     action_msg = tcp_response.get("action_msg")
     action_log = tcp_response.get("action_log")
     # TODO: assert all of the above. deal with edge cases?
