efd1b2b7dae8e3f9a8753ef28aa13b53b0045aa8
tginsbu1 <tsg44@cornell.edu>
2023-06-15 16:27:47 -0500

    improving docs

--- tests/test_wei_locations.py
+++ tests/test_wei_locations.py
@@ -9,11 +9,11 @@ class TestWEI_Locations(TestWEI_Base):
         from rpl_wei.core.workflow import WorkflowRunner
         from rpl_wei.core.workcell import Workcell
 
-        workcell_config_path = Path("tests/test_pcr_workcell.yaml")
+        workcell_config_path = Path("tests/test_workcell.yaml")
         workcell_def = yaml.safe_load(workcell_config_path.read_text())
         workcell = Workcell(workcell_def)
 
-        workflow_config_path = Path("tests/test_pcr_workflow.yaml")
+        workflow_config_path = Path("tests/test_workflow.yaml")
         worfklow_def = yaml.safe_load(workflow_config_path.read_text())
         runner = WorkflowRunner(worfklow_def, "test_experiment")
         workflow = runner.workflow
