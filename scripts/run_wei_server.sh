70da5308afc6c4e5ef8b4b67dabbb6ae41bfc8a6
tginsbu1 <73187720+tginsbu1@users.noreply.github.com>
2023-06-16 13:01:21 -0500

    improving docs (#30)
    * improving docs

--- scripts/run_wei_server.sh
+++ scripts/run_wei_server.sh
@@ -19,7 +19,7 @@ tmux send-keys -t $session:$window 'python3 -m rpl_wei.processing.worker' C-m
 window=2
 tmux new-window -t $session:$window -n 'server'
 tmux send-keys -t $session:$window 'cd ' $folder C-m
-tmux send-keys -t $session:$window 'python3 -m rpl_wei.server --workcell ~/workspace/BIO_workcell/bio_workcell/bio_workcell.yaml' C-m
+tmux send-keys -t $session:$window 'python3 -m rpl_wei.server --workcell tests/test_workcell.yaml' C-m
 
 tmux attach-session -t $session
 
