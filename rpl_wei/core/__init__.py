3b5bf890f577c4fea18306948d8d4f56ffe07396
tginsbu1 <tsg44@cornell.edu>
2023-06-22 13:15:20 -0500

    merging docs

--- rpl_wei/core/__init__.py
+++ rpl_wei/core/__init__.py
@@ -1,6 +1,4 @@
 """The module responsible for the code for maddnaging and executing WEI flows"""
 
-"""The module responsible for the code for managing and executing WEI flows"""
-
 from pathlib import Path
 DATA_DIR = Path.home() / ".wei"
