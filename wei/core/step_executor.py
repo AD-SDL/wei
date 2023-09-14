f3bb8d2cdb4993d74529f3a8071d21aa1130ac28
Ryan Lewis <accounts+github@ryandlewis.dev>
2023-09-14 16:10:42 -0500

    Refactor: remove merge artifacts commited by mistake

--- wei/core/step_executor.py
+++ wei/core/step_executor.py
@@ -3,37 +3,13 @@ import logging
 from typing import Optional
 
 from wei.core.data_classes import Module, Step, StepStatus
-<<<<<<< HEAD
-from wei.core.interfaces.rest_interface import RestInterface
-from wei.core.interfaces.ros2_interface import ROS2Interface, wei_ros2_camera_callback
-from wei.core.interfaces.simulate_interface import silent_callback
-from wei.core.interfaces.tcp_interface import TcpInterface
-from wei.core.interfaces.zmq_interface import ZMQInterface
-=======
 from wei.core.interface import Interface_Map
->>>>>>> 463c9ad49e4f7bfd92d232746715f95674d581f9
 
 ########################
 #   Executor mapping   #
 ########################
 
 
-<<<<<<< HEAD
-class Executor_Map:
-    """Mapping of YAML names to functions from interfaces"""
-
-    function = {
-        "wei_ros_node": ROS2Interface,
-        "wei_ros_camera": wei_ros2_camera_callback,
-        "wei_tcp_node": TcpInterface,
-        "wei_rest_node": RestInterface,
-        "wei_zmq_node": ZMQInterface,
-        "simulate_callback": silent_callback,
-    }
-
-
-=======
->>>>>>> 463c9ad49e4f7bfd92d232746715f95674d581f9
 class StepExecutor:
     """Class to handle executing steps"""
 
