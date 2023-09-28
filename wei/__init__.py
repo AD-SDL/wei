dd9d2b1770e7abb3fcc8b645ae6f20c56a601dc1
Ryan Lewis <accounts+github@ryandlewis.dev>
2023-09-28 16:38:11 -0500

    Bump version to 0.3.1

--- wei/__init__.py
+++ wei/__init__.py
@@ -1,4 +1,4 @@
 """A system for orchestrating the actions of multiple robots and scientific intruments to autonomosly perform generalized experiments"""
-__version__ = 0.2
+__version__ = "0.3.1"
 
 from wei.exp_app import Experiment  # noqa
