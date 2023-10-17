2bddc8900a7634e3283ae8fb4c654e49377e87b5
Ryan Lewis <accounts+github@ryandlewis.dev>
2023-10-17 13:30:33 -0500

    Refactor: Autoformat and linting

--- tests/test_wei_locations.py
+++ tests/test_wei_locations.py
@@ -8,19 +8,13 @@ class TestWEI_Locations(TestWEI_Base):
     def test_workflow_replace_locations(self):
         from pathlib import Path
 
-        from wei.core.workcell import Workcell
         from wei.core.workflow import WorkflowRunner
-
-        workcell_config_path = Path("tests/test_workcell.yaml")
-        workcell_def = yaml.safe_load(workcell_config_path.read_text())
-        workcell = Workcell(workcell_def)
 
         workflow_config_path = Path("tests/test_workflow.yaml")
         workflow_def = yaml.safe_load(workflow_config_path.read_text())
         arg_before_replace = workflow_def["flowdef"][1]["args"]
         self.assertEqual(arg_before_replace["source"], "webcam.pos")
         self.assertEqual(arg_before_replace["target"], "webcam.pos")
-
 
         # Changes happen during the creation of workflow runner
         runner = WorkflowRunner(
@@ -32,9 +26,8 @@ class TestWEI_Locations(TestWEI_Base):
             simulate=True,
             workflow_name="Test Workflow",
         )
-        workflow = runner.workflow
 
-        arg_after_replace = workflow.flowdef[1].args
+        arg_after_replace = runner.workflow.flowdef[1].args
         self.assertListEqual(
             arg_after_replace["source"],
             [0, 0, 0, 0, 0, 0],
