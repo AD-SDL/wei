81eedc39330bc208a9cb8a7f135c5f9b44575c4a
Ryan Lewis <accounts+github@ryandlewis.dev>
2023-12-04 17:08:55 -0600

    v0.5.1.dev0 test release

--- wei/__init__.py
+++ wei/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 """A system for orchestrating the actions of multiple robots and scientific instruments to autonomously perform generalized experiments"""
-__version__ = "0.5.0"
+__version__ = "0.5.1.dev0"
 
 # Alias "ExperimentClient" -> "Experiment" for backwards compatibility
 from wei.experiment_client import ExperimentClient
