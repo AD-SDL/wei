965d79413c8e8d2e498a84a037b37f8e4c2888e9
Dozgulbas <dozgulbas@anl.gov>
2024-06-19 14:25:17 -0700

    Adding smtp server to workcell config

--- src/wei/types/workcell_types.py
+++ src/wei/types/workcell_types.py
@@ -88,6 +88,7 @@ class WorkcellConfig(BaseModel, extra="allow"):
     cold_start_delay: int = Field(
         default=2, description="Delay before starting the engine"
     )
+    smtp_server: str = Field(default="0.0.0.0", description="Hostname for the WEI server")
 
     # Validators
     @field_validator("data_directory")
