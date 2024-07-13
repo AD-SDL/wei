5ae5cb593cf448d3ffc38d984d006ae682dd50e8
Dozgulbas <dozgulbas@anl.gov>
2024-07-12 19:00:34 -0700

    Testing resources endpoint

---
+++ tests/test_resources.py
@@ -0,0 +1,18 @@
+"""Test resource endpoint"""
+
+import requests
+
+
+def get_resources():
+    """Get resources"""
+    url = "http://localhost:2000/resources"
+    response = requests.get(url)
+    if response.status_code == 200:
+        resources = response.json()
+        print("Resources:", resources)
+    else:
+        print(f"Failed to get resources: {response.status_code}")
+
+
+if __name__ == "__main__":
+    get_resources()
