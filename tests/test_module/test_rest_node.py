1f7fa8ed6e934c21da13f3a67bbf3b1f90071490
Dozgulbas <dozgulbas@anl.gov>
2024-07-16 19:26:18 -0700

    Working on the bug with resoruces

--- tests/test_module/test_rest_node.py
+++ tests/test_module/test_rest_node.py
@@ -46,46 +46,39 @@ test_rest_node.arg_parser.add_argument(
 )
 
 
-@test_rest_node.resource()
-def stack_resource():
-    """Stack type of resource"""
-    return StackQueue(
-        information="Stack for transfer",
-        name="StackResource",
-        capacity=10,
-        quantity=3,
-        contents=[Asset(name="Plate1"), Asset(name="Plate2"), Asset(name="Plate3")],
-    )
+# Define and add resources
+test_rest_node.state.stack_resource = StackQueue(
+    information="Stack for transfer",
+    name="StackResource",
+    capacity=10,
+    quantity=3,
+    contents=[Asset(name="Plate1"), Asset(name="Plate2"), Asset(name="Plate3")],
+)
+test_rest_node.resources.append(test_rest_node.state.stack_resource)
 
+test_rest_node.state.pool_collection_resource = PoolCollection(
+    name="Plate1",
+    wells={
+        f"{row}{col}": Pool(
+            information=f"Well {row}{col}",
+            name=f"Well{row}{col}",
+            capacity=100.0,
+            quantity=50.0,
+            contents={"description": "Yellow ink", "quantity": 50.0},
+        )
+        for row in "ABCDEFGH"
+        for col in range(1, 13)
+    },
+)
+test_rest_node.resources.append(test_rest_node.state.pool_collection_resource)
 
-@test_rest_node.resource()
-def pool_collection_resource():
-    """Pool collection type of resource"""
-    return PoolCollection(
-        name="Plate1",
-        wells={
-            f"{row}{col}": Pool(
-                information=f"Well {row}{col}",
-                name=f"Well{row}{col}",
-                capacity=100.0,
-                quantity=50.0,
-                contents={"description": "Yellow ink", "quantity": 50.0},
-            )
-            for row in "ABCDEFGH"
-            for col in range(1, 13)
-        },
-    )
-
-
-@test_rest_node.resource()
-def collection_resource():
-    """Collection type of resource"""
-    return Collection(
-        information="Collection for measurement",
-        name="CollectionResource",
-        capacity=5,
-        quantity=2,
-    )
+test_rest_node.state.collection_resource = Collection(
+    information="Collection for measurement",
+    name="CollectionResource",
+    capacity=5,
+    quantity=2,
+)
+test_rest_node.resources.append(test_rest_node.state.collection_resource)
 
 
 @test_rest_node.startup()
@@ -113,10 +106,9 @@ def transfer(
     asset = (
         Asset(name=plate_name)
         if plate_name
-        else Asset(name=f"Plate{len(stack_resource.contents) + 1}")
+        else Asset(name=f"Plate{len(test_rest_node.resources[0].contents) + 1}")
     )
-    stack_resource.push(asset)
-    print(stack_resource.contents)
+    test_rest_node.resources[0].push(asset)
     print(test_rest_node.resources)
     return StepResponse.step_succeeded(f"Moved sample from {source} to {target}")
 
@@ -133,10 +125,10 @@ def synthesize(
     protocol = protocol.file.read().decode("utf-8")
     print(protocol)
 
-    pool_collection_resource.wells["A1"].increase(foo)
-    pool_collection_resource.wells["B1"].decrease(bar)
-    pool_collection_resource.wells["C1"].fill()
-    pool_collection_resource.wells["D1"].empty()
+    test_rest_node.resources[1].wells["A1"].increase(foo)
+    test_rest_node.resources[1].wells["B1"].decrease(bar)
+    test_rest_node.resources[1].wells["C1"].fill()
+    test_rest_node.resources[1].wells["D1"].empty()
 
     new_plate_contents = {
         "A2": {"description": "Red ink", "quantity": 10.0},
@@ -144,8 +136,7 @@ def synthesize(
         "C2": {"description": "Green ink", "quantity": 30.0},
         "D2": {"description": "Yellow ink", "quantity": 40.0},
     }
-    pool_collection_resource.update_plate(new_plate_contents)
-    print(pool_collection_resource.wells)
+    test_rest_node.resources[1].update_plate(new_plate_contents)
     print(test_rest_node.resources)
 
     return StepResponse.step_succeeded(f"Synthesized sample {foo} + {bar}")
@@ -155,13 +146,13 @@ def synthesize(
 def measure_action(state: State, action: ActionRequest) -> StepResponse:
     """Measures the foobar of the current sample"""
 
-    print(collection_resource.quantity)
-    print(collection_resource.contents)
+    print(test_rest_node.resources[2].quantity)
+    print(test_rest_node.resources[2].contents)
     instance = {"measurement": state.foobar}
-    location = f"location_{len(collection_resource.contents)+1}"
-    collection_resource.insert(location, instance)
-    print(collection_resource.quantity)
-    print(collection_resource.contents)
+    location = f"location_{len(test_rest_node.resources[2].contents)+1}"
+    test_rest_node.resources[2].insert(location, instance)
+    print(test_rest_node.resources[2].quantity)
+    print(test_rest_node.resources[2].contents)
 
     with open("test.txt", "w") as f:
         f.write("test")
