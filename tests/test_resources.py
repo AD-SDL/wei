5271e41faf09329c6a62a5c6cfa9d1e3bb29a533
Dozgulbas <dozgulbas@anl.gov>
2024-07-22 18:30:07 -0700

    Working on the bugs with resources action end points

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -2,6 +2,90 @@
 
 import requests
 
+from wei.types.resource_types import Asset
+
+BASE_URL = (
+    "http://localhost:2001"  # Adjust this to match your server's address if different
+)
+
+
+def test_get_state():
+    """"""
+    response = requests.get(f"{BASE_URL}/state")
+    print("GET /state")
+    print(response.json())
+
+
+def test_get_resources():
+    """"""
+
+    response = requests.get(f"{BASE_URL}/resources")
+    print("GET /resources")
+    print(response.json())
+
+
+def test_push_resource():
+    """"""
+
+    resource_id = "01J3EK2AECBW0JCP3B0P38Q7K5"  # Replace with an actual resource ID
+    asset = Asset(name="TEST_PLATE")
+    response = requests.post(
+        f"{BASE_URL}/resources/{resource_id}/push", json=asset.model_dump()
+    )
+    print(f"POST /resources/{resource_id}/push")
+    print(response.json())
+
+
+def test_pop_resource():
+    """"""
+
+    resource_id = "Stack1"  # Replace with an actual resource ID
+    response = requests.post(f"{BASE_URL}/resources/{resource_id}/pop")
+    print(f"POST /resources/{resource_id}/pop")
+    print(response.json())
+
+
+def test_increase_pool():
+    """"""
+
+    resource_id = "Plate1"  # Replace with an actual resource ID
+    well_id = "A1"  # Replace with an actual well ID
+    amount = {"amount": 10}
+    response = requests.post(
+        f"{BASE_URL}/resources/{resource_id}/wells/{well_id}/increase", json=amount
+    )
+    print(f"POST /resources/{resource_id}/wells/{well_id}/increase")
+    print(response.json())
+
+
+def test_decrease_pool():
+    """"""
+    resource_id = "Plate1"  # Replace with an actual resource ID
+    well_id = "A1"  # Replace with an actual well ID
+    amount = {"amount": 5}
+    response = requests.post(
+        f"{BASE_URL}/resources/{resource_id}/wells/{well_id}/decrease", json=amount
+    )
+    print(f"POST /resources/{resource_id}/wells/{well_id}/decrease")
+    print(response.json())
+
+
+def test_write_resources():
+    """"""
+    response = requests.post(f"{BASE_URL}/resources/write")
+    print("POST /resources/write")
+    print(response.json())
+
+
+# if __name__ == "__main__":
+#     test_get_state()
+#     test_get_resources()
+#     test_push_resource()
+#     test_pop_resource()
+#     test_increase_pool()
+#     test_decrease_pool()
+#     test_write_resources()
+
 
 def get_resources():
     """Get resources"""
@@ -15,4 +99,6 @@ def get_resources():
 
 
 if __name__ == "__main__":
-    get_resources()
+    # get_resources()
+    test_push_resource()
+    # test_increase_pool()
