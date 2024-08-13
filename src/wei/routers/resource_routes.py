1cecdb965732deeb4a9fe1724e65aae8177ec7c8
Dozgulbas <dozgulbas@anl.gov>
2024-08-12 17:23:07 -0700

    Working on converting the server to router

--- src/wei/routers/resource_routes.py
+++ src/wei/routers/resource_routes.py
@@ -1,10 +1,8 @@
 """API server for the Resource Manager"""
 
-from argparse import ArgumentParser
-from contextlib import asynccontextmanager
 from typing import Dict, List
 
-from fastapi import FastAPI
+from fastapi import APIRouter
 
 from wei.resources_interface import ResourceInterface
 from wei.types.resource_types import (
@@ -17,50 +15,21 @@ from wei.types.resource_types import (
     StackTable,
 )
 
+router = APIRouter()
 database_url = "sqlite:///database.db"
 
 
-@asynccontextmanager  # type: ignore
-async def lifespan(app: FastAPI) -> None:  # type: ignore[misc]
+def get_resource_interface() -> ResourceInterface:
     """
-    Initial run function for the app, parses the workcell argument
-
-    Parameters
-    ----------
-    app : FastApi
-        The REST API app being initialized
-    Returns
-    -------
-    None
-    """
-
-    # Initialize the database and create tables
-    resource_interface = ResourceInterface(database_url)
-    app.state.resource_interface = resource_interface
-
-    # Yield control to the application
-    yield
-
-    # Do any cleanup here
-    pass
-
-
-app = FastAPI(
-    lifespan=lifespan,
-)
-
-
-def get_resource_interface():
-    """
-    Dependency function to get the ResourceInterface from app state.
+    Dependency function to get the ResourceInterface.
 
     Returns:
         ResourceInterface: The resource interface with the default database URL.
     """
-    return app.state.resource_interface
+    return ResourceInterface(database_url)
 
 
-@app.get("/up")
+@router.get("/up")
 def is_server_up() -> Dict[str, bool]:
     """
     Check if the resource server is up
@@ -68,7 +37,7 @@ def is_server_up() -> Dict[str, bool]:
     return {"up": True}
 
 
-@app.get("/resources/{resource_type}")
+@router.get("/resources/{resource_type}")
 def get_all_resources(
     resource_type: str,
 ):  # TODO: Make this get resources by type & create a new function for get all resources
@@ -81,7 +50,7 @@ def get_all_resources(
     Returns:
         List[ResourceContainer]: List of all resources of the specified type.
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     resource_map = {
         "pool": PoolTable,
         "stack": StackTable,
@@ -96,23 +65,23 @@ def get_all_resources(
         return {"error": f"Invalid resource type: {resource_type}"}
 
 
-@app.get("/assets")
+@router.get("/assets")
 def get_all_assets() -> List[AssetTable]:
     """
     Get all available assets
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     return resource_interface.get_all_resources(AssetTable)
 
 
-@app.get("/resource/{resource_type}/{resource_id}")
+@router.get("/resource/{resource_type}/{resource_id}")
 def get_resource_by_id(
     resource_type: str, resource_id: str
 ):  # TODO: don't use resource type
     """
     Get a resource by its ID and type.
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     resource_map = {
         "pools": PoolTable,
         "stacks": StackTable,
@@ -127,23 +96,23 @@ def get_resource_by_id(
         return {"error": f"Invalid resource type: {resource_type}"}
 
 
-@app.get("/asset/{asset_id}")
+@router.get("/asset/{asset_id}")
 def get_asset_by_id(asset_id: str) -> AssetTable:
     """
     Get an asset by its ID
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     return resource_interface.get_resource(AssetTable, asset_id)
 
 
-@app.post("/resource/{resource_type}")
+@router.post("/resource/{resource_type}")
 def create_resource(
     resource_type: str, resource: ResourceContainerBase
 ):  # TODO: resource needs to be updated
     """
     Create a new resource of a specific type.
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     resource_map = {
         "pools": PoolTable,
         "stacks": StackTable,
@@ -158,30 +127,30 @@ def create_resource(
         return {"error": f"Invalid resource type: {resource_type}"}
 
 
-@app.post("/asset")
+@router.post("/asset")
 def create_asset(asset: AssetTable) -> AssetTable:
     """
     Create a new asset
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     return resource_interface.add_resource(asset)
 
 
-@app.put("/asset/{asset_id}")
+@router.put("/asset/{asset_id}")
 def update_asset(asset_id: str, asset: AssetTable) -> AssetTable:
     """
     Update an asset
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     return resource_interface.update_resource(AssetTable, asset_id, asset.dict())
 
 
-@app.put("/resource/{resource_type}/{resource_id}")
+@router.put("/resource/{resource_type}/{resource_id}")
 def update_resource(resource_type: str, resource_id: str, updates: Dict[str, any]):
     """
     Update a resource by its ID and type.
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     resource_map = {
         "pools": PoolTable,
         "stacks": StackTable,
@@ -196,12 +165,12 @@ def update_resource(resource_type: str, resource_id: str, updates: Dict[str, any
         return {"error": f"Invalid resource type: {resource_type}"}
 
 
-@app.delete("/resource/{resource_type}/{resource_id}")
+@router.delete("/resource/{resource_type}/{resource_id}")
 def delete_resource(resource_type: str, resource_id: str):
     """
     Delete a resource by its ID and type.
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     resource_map = {
         "pools": PoolTable,
         "stacks": StackTable,
@@ -218,21 +187,21 @@ def delete_resource(resource_type: str, resource_id: str):
         return {"error": f"Invalid resource type: {resource_type}"}
 
 
-@app.delete("/asset/{asset_id}")
+@router.delete("/asset/{asset_id}")
 def delete_asset(asset_id: str) -> AssetTable:
     """
     Delete an asset
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     return {"deleted": resource_interface.delete_resource(AssetTable, asset_id)}
 
 
-@app.put("/resources/{resource_id}/push")
+@router.put("/resources/{resource_id}/push")
 def push_asset_to_resource(resource_id: str, asset: AssetTable) -> int:
     """
     Push an asset to a stack or queue resource
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     resource_type = resource_interface.get_resource_type(resource_id)
     if resource_type == "StackTable":
         stack = resource_interface.get_resource(StackTable, resource_id)
@@ -244,12 +213,12 @@ def push_asset_to_resource(resource_id: str, asset: AssetTable) -> int:
         return {"error": f"Invalid resource type for push operation: {resource_type}"}
 
 
-@app.put("/resources/{resource_id}/pop")
+@router.put("/resources/{resource_id}/pop")
 def pop_asset_from_resource(resource_id: str) -> AssetTable:
     """
     Pop an asset from a stack or queue resource
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     resource_type = resource_interface.get_resource_type(resource_id)
     if resource_type == "StackTable":
         stack = resource_interface.get_resource(StackTable, resource_id)
@@ -261,53 +230,53 @@ def pop_asset_from_resource(resource_id: str) -> AssetTable:
         return {"error": f"Invalid resource type for pop operation: {resource_type}"}
 
 
-@app.put("/resources/{resource_id}/increase")
+@router.put("/resources/{resource_id}/increase")
 def increase_resource_quantity(resource_id: str, quantity: float) -> float:
     """
     Increase the quantity in a Pool resource
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     pool = resource_interface.get_resource(PoolTable, resource_id)
     resource_interface.increase_pool_quantity(pool, quantity)
     return pool.quantity
 
 
-@app.put("/resources/{resource_id}/decrease")
+@router.put("/resources/{resource_id}/decrease")
 def decrease_resource_quantity(resource_id: str, quantity: float) -> float:
     """
     Decrease the quantity in a Pool resource
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     pool = resource_interface.get_resource(PoolTable, resource_id)
     resource_interface.decrease_pool_quantity(pool, quantity)
     return pool.quantity
 
 
-@app.put("/resources/{resource_id}/insert")
+@router.put("/resources/{resource_id}/insert")
 def insert_asset_into_resource(
     resource_id: str, location: str, asset: AssetTable
 ) -> int:
     """
     Insert an asset into a Collection resource
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     collection = resource_interface.get_resource(CollectionTable, resource_id)
     resource_interface.insert_into_collection(collection, location, asset)
     return len(collection.contents_dict)
 
 
-@app.put("/resources/{resource_id}/retrieve")
+@router.put("/resources/{resource_id}/retrieve")
 def remove_asset_from_resource(resource_id: str, location: str) -> Dict[str, str]:
     """
     Retrieve an asset from a Collection resource
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     collection = resource_interface.get_resource(CollectionTable, resource_id)
     asset = resource_interface.retrieve_from_collection(collection, location)
     return {"id": asset.id, "name": asset.name}
 
 
-@app.put("/resources/{resource_id}/empty")
+@router.put("/resources/{resource_id}/empty")
 def empty_pool_resource(resource_id: str) -> float:
     """
     Empty a Pool resource by setting its quantity to zero.
@@ -318,13 +287,13 @@ def empty_pool_resource(resource_id: str) -> float:
     Returns:
         float: The new quantity of the pool (which should be 0).
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     pool = resource_interface.get_resource(PoolTable, resource_id)
     resource_interface.empty_pool(pool)
     return pool.quantity
 
 
-@app.put("/resources/{resource_id}/fill")
+@router.put("/resources/{resource_id}/fill")
 def fill_pool_resource(resource_id: str) -> float:
     """
     Fill a Pool resource by setting its quantity to its capacity.
@@ -335,35 +304,7 @@ def fill_pool_resource(resource_id: str) -> float:
     Returns:
         float: The new quantity of the pool (which should be its capacity).
     """
-    resource_interface: ResourceInterface = app.state.resource_interface
+    resource_interface: ResourceInterface = router.state.resource_interface
     pool = resource_interface.get_resource(PoolTable, resource_id)
     resource_interface.fill_pool(pool)
     return pool.quantity
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    parser = ArgumentParser()
-    parser.add_argument(
-        "--host", type=str, help="Host for the resource server", default="0.0.0.0"
-    )
-    parser.add_argument(
-        "--port", type=str, help="Port for the resource server", default="8001"
-    )
-    parser.add_argument(
-        "--database-url",
-        type=str,
-        help="Database URL for the resource server",
-        default="sqlite:///database.db",
-    )
-    args = parser.parse_args()
-
-    resource_interface = ResourceInterface(database_url=args.database_url)
-
-    uvicorn.run(
-        "wei.resource_server:app",
-        host=args.host,
-        port=args.port,
-        reload=False,
-    )
